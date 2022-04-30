82f3b0c0636dbda9b44c7722dc12ff68ba035bda
Devon Faria <devonfaria@gmail.com>
2022-04-30 11:58:53 -0400

    corrected typeDefs to reflect models, queries, and mutations for this app

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,19 +2,38 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    bookCount: 
+    savedBooks: [String]
+  }
 
+  type Book {
+    authors: [String]
+    description: String!
+    bookId: ID!
+    image: String
+    link: String
+    title: String!
+  
   }
 
   type Auth {
-
+    token: ID!
+    user: User
   }
 
   type Query {
-
+    me: User
   }
 
   type Mutation {
-  
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(profileId: ID!, skill: String!): User
+    removeBook(bookId: String!): User
   }
 `;
 
